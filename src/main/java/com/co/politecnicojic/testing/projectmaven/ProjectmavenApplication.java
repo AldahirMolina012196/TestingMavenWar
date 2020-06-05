54f58162c66b7433782fb3e9eca64710c10f7660
Aldahir M <aldahir_molina82161@elpoli.edu.co>
2020-06-05 16:12:29 -0500

    Add new repository for testing maven

---
+++ src/main/java/com/co/politecnicojic/testing/projectmaven/ProjectmavenApplication.java
@@ -0,0 +1,13 @@
+package com.co.politecnicojic.testing.projectmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/politecnicojic/testing/projectmaven/ProjectmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.politecnicojic.testing.projectmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
